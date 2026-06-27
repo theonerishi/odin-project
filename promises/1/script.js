b7efbe6f697ea4f9dbc7fa349f0b8c364aee0fb3
Rishi <itfl@yahoo.com>
2026-06-27 20:52:49 +0100

    working through async

--- promises/1/script.js
+++ promises/1/script.js
@@ -5,3 +5,8 @@ let promise = new Promise(function(resolve, reject) {
 })
 promise.then(alert);
 // the output is 1 because further calls are ignored
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    
+}
+delay(3000).then(() => alert('runs after 3 seconds'));
