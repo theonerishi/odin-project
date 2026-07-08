6fd843a75df87b9d00fac10011fbbbeb88c5db05
theonerishi <135510283+theonerishi@users.noreply.github.com>
2026-07-08 14:20:18 +0000

    recursion

---
+++ recursion/js-info/sum-all/for-loop/script.js
@@ -0,0 +1,12 @@
+let x;
+let sum = 0;
+
+do {
+    x = Number(prompt("enter a number"));
+} while (Number.isNaN(x) || x < 0);
+
+for (let i = 1; i <= x; i++) {
+    sum += i;
+}
+
+alert(sum);
---
+++ recursion/js-info/sum-all/recursion/script.js
@@ -0,0 +1,13 @@
+let x;
+let sum = 0;
+do {
+    x = Number(prompt('enter a number'));
+} while ( x < 0 || Number.isNaN(x))
+function sumTo(n) {
+    if (n == 1) {
+        return n;
+    } else {
+        return n + sumTo(n-1);
+    }
+}
+alert(sumTo(x));
